6e487b73c7659cc6b9b52e3d747e389fca8008bf
cainamicael <cainamicaeloficial@gmail.com>
2023-06-27 10:30:58 -0300

    add

--- Códigos/React/aprendendo_react/src/App.js
+++ Códigos/React/aprendendo_react/src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import React from 'react'
 import Api from './Api.js'
+import Search from './Search.js'
 
 function App() {
 
   return (
-    <Api></Api>
+    <Search />
   )
 
 }
---
+++ Códigos/React/aprendendo_react/src/Search.js
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Search() {
+
+    function searchInput() {
+        let currentValue = document.querySelector('input[name=searchInput]').value
+
+        
+    }
+
+    return(
+        <div className="search">
+            <h2>Digite a cidade que você quer saber a previsão...</h2>
+            <input type="text" name="searchInput" placeholder="Digite a cidade" onKeyUp={searchInput}/>
+        </div>
+    )
+
+}
+
+export default Search
