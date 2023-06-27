9d83c20414ed6f7eb751951bb725c9de39e9f324
cainamicael <cainamicaeloficial@gmail.com>
2023-06-26 22:32:40 -0300

    add

--- Códigos/React/aprendendo_react/src/App.js
+++ Códigos/React/aprendendo_react/src/App.js
@@ -1,26 +1,11 @@
 import './App.css';
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import React from 'react'
+import Api from './Api.js'
 
 function App() {
 
-  const [nomes, setNomes] = useState([])
-
-  useEffect(() => {
-    axios.get('https://jsonplaceholder.typicode.com/users')
-      .then(res => setNomes(res.data))
-  }, [])
-
-  return(
-    <div>
-      {nomes.map((res, index) => {
-        return(
-          <div>
-            <h2 key={index}>{res.name} | {res.email}</h2>
-          </div>
-        )
-      })}
-    </div>
+  return (
+    <Api></Api>
   )
 
 }
